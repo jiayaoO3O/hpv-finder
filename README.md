8106508d59f8ee24e868abddf1be0e5f06db3c1f
jiayao <jiayao3.14@outlook.com>
2021-10-25 12:07:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# hpv-finder Project
+
+hpv疫苗爬虫项目, 使用quarkus框架, 正在开发中.
---
+++ src/main/java/io/github/jiayaoO3O/task/HpvTask.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 jiayao hpv-finder is licensed under Mulan PSL v2. You can use this software according to the terms and conditions of the Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the Mulan PSL v2 for more details.
+ */
+
+package io.github.jiayaoO3O.task;
+
+import cn.hutool.log.Log;
+import io.quarkus.scheduler.Scheduled;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Created by jiayao on 2021/10/25.
+ */
+@ApplicationScoped
+public class HpvTask {
+    private static final Log log = Log.get();
+    @Scheduled(every = "1s")
+    public void check(){
+        log.info("hello world");
+    }
+}
