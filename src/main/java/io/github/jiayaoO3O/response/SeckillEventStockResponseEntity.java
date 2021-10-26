65e03f9e96bed3ce850bc9df54247106a1f7686a
jiayao <jiayao3.14@outlook.com>
2021-10-26 15:30:43 +0800

    feat: 添加秒苗活动库存Response

---
+++ src/main/java/io/github/jiayaoO3O/response/SeckillEventStockResponseEntity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 jiayao hpv-finder is licensed under Mulan PSL v2. You can use this software according to the terms and conditions of the Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the Mulan PSL v2 for more details.
+ */
+
+package io.github.jiayaoO3O.response;
+
+import io.github.jiayaoO3O.entity.BaseResponseEntity;
+import io.github.jiayaoO3O.entity.miaomiao.SeckillEventStockEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by jiayao on 2021/10/26.
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SeckillEventStockResponseEntity extends BaseResponseEntity {
+    private SeckillEventStockEntity data;
+}
